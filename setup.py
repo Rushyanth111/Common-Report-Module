9265096cee1a3d3d38ba5be76bd5965511346a5a
Rushyanth S <42034084+Rushyanth111@users.noreply.github.com>
2020-08-29 11:32:35 +0530

    Added __init__ and setup for a library.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages,
+
+setup(
+    name="common-report-module",
+    description="A Helper Module for Semester-Stats",
+    author="Rushyanth S",
+    author_email="42034084+Rushyanth111@users.noreply.github.com",
+    python_requires='>=3.7.0',
+    url="https://github.com/Rushyanth111/Common-Report-Module",
+    packages=find_packages(exclude=[".venv", "*tests*"]),
+    install_requires=["pydantic", "requests"],
+    extras_require=["pytest"]
+)
