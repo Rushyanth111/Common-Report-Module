58c402b94e3387a8beef678e0ba87e4b9df6d0ec
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-08-30 18:08:35 +0530

    Added RestClient.

--- semester_stats_report/httpclient.py
+++ semester_stats_report/httpclient.py
@@ -1,15 +1,16 @@
 import requests
+from typing import Any, Dict
 
 
 class RestClient:
     def __init__(self, url=None, port=None) -> None:
         self.url = "https://localhost:8000" if url is not None else url
 
-    def _get():
-        pass
+    def _get(self, params: Dict[str, str] = None) -> requests.Response:
+        return requests.get(self.url, params=params)
 
-    def _post():
-        pass
+    def _post(self, body: Any):
+        return requests.post(self.url, data=body)
 
-    def _put():
-        pass
+    def _put(self, body: Any):
+        return requests.put(self.url, data=body)
