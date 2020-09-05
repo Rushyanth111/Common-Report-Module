9271cdc9475048c1cf8c397051deb162080e4a5b
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-05 23:59:49 +0530

    Added bulk Method + Fixed Other Clients Inits

--- semester_stats_report/client.py
+++ semester_stats_report/client.py
@@ -2,6 +2,7 @@ from .batchclient import BatchClient
 from .deptclient import DeptClient
 from .studentclient import StudentClient
 from .subjectclient import SubjectClient
+from .bulkclient import BulkClient
 
 
 class SemesterClient:
@@ -12,10 +13,13 @@ class SemesterClient:
         return BatchClient(self.url, batch)
 
     def dept(self, dept: str) -> DeptClient:
-        return DeptClient(self.url)
+        return DeptClient(self.url, dept)
 
     def student(self, usn: str) -> StudentClient:
-        return StudentClient(self.url)
+        return StudentClient(self.url, usn)
 
     def subject(self, subcode: str) -> SubjectClient:
-        return SubjectClient(self.url)
+        return SubjectClient(self.url, subcode)
+
+    def bulk(self):
+        return BulkClient(self.url)
