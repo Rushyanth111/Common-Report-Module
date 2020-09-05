8cde4c83f4ac89df3433546870d4e53456d6056e
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-05 23:59:06 +0530

    Added Bulk Client.

---
+++ semester_stats_report/bulkclient.py
@@ -0,0 +1,25 @@
+from semester_stats_report.reports import (
+    DepartmentReport,
+    ScoreReport,
+    StudentReport,
+    SubjectReport,
+)
+from .httpclient import BaseClient
+from typing import List
+
+
+class BulkClient(BaseClient):
+    def __init__(self, url: str) -> None:
+        super().__init__(url + "/bulk")
+
+    def scores(self, reports: List[ScoreReport]):
+        return self._post("/score", body=[x.dict() for x in reports])
+
+    def dept(self, reports: List[DepartmentReport]):
+        return self._post("/dept", body=[x.dict() for x in reports])
+
+    def subject(self, reports: List[SubjectReport]):
+        return self._post("/subject", body=[x.dict() for x in reports])
+
+    def student(self, reports: List[StudentReport]):
+        return self._post("/student", body=[x.dict() for x in reports])
