e147a726084093747d8b94d5702963747d57c25c
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-09-06 00:51:01 +0530

    Client::SemesterClient: Added DocStrings.

--- semester_stats_report/client.py
+++ semester_stats_report/client.py
@@ -6,19 +6,59 @@ from .bulkclient import BulkClient
 
 
 class SemesterClient:
+    """The Base Client for All API Manipulation."""
+
     def __init__(self, url: str) -> None:
+        """Obtain the API Client that Can Interact
+            with the Server at the given URL.
+
+        Args:
+            url (str): The url of the server
+        """
         self.url = url
 
     def batch(self, batch: int) -> BatchClient:
+        """Obtain the Client wrt to /Batch/
+
+        Args:
+            batch (int): Integer Batch to Obtain
+
+        Returns:
+            BatchClient: A Client that Handles Batch Operations
+        """
         return BatchClient(self.url, batch)
 
     def dept(self, dept: str) -> DeptClient:
+        """[summary]
+
+        Args:
+            dept (str): [description]
+
+        Returns:
+            DeptClient: [description]
+        """
         return DeptClient(self.url, dept)
 
     def student(self, usn: str) -> StudentClient:
+        """[summary]
+
+        Args:
+            usn (str): [description]
+
+        Returns:
+            StudentClient: [description]
+        """
         return StudentClient(self.url, usn)
 
     def subject(self, subcode: str) -> SubjectClient:
+        """[summary]
+
+        Args:
+            subcode (str): [description]
+
+        Returns:
+            SubjectClient: [description]
+        """
         return SubjectClient(self.url, subcode)
 
     def bulk(self):
