e255408a4305972c1fb2d5f15731c0545bebacf4
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-11 18:57:37 +0530

    DeptClient: Added Docs.

--- semester_stats_report/deptclient.py
+++ semester_stats_report/deptclient.py
@@ -5,18 +5,39 @@ from .httpclient import BaseClient
 
 class DeptClient(BaseClient):
     def __init__(self, url: str, dept: str) -> None:
+        """Client WRT to /dept/ Route
+
+        Args:
+            url (str): Url of the Server
+            dept (str): Department Required.
+        """
         super().__init__(url + "/dept")
         self.dept = dept
 
-    def get(self):
+    def get(self) -> DepartmentReport:
+        """Get the Department
+
+        Returns:
+            DepartmentReport: Deparment Report with Details.
+        """
         res = self._get("/{}".format(self.dept))
         rec = DepartmentReport.parse_obj(res)
         return rec
 
     def update(self, report: DepartmentReport):
+        """Update a Department
+
+        Args:
+            report (DepartmentReport): Department Report
+        """
         res = self._put("/{}".format(self.dept), body=report.dict())
         return res
 
     def add(self, report: DepartmentReport):
+        """Send a Department
+
+        Args:
+            report (DepartmentReport): Department Report
+        """
         res = self._post("/", body=report.dict())
         return res
