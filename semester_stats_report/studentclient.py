295e8453c41f0f0cf81b76fd9d37fcbb6c9c9397
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-11 18:30:51 +0530

    StudentClient: Updated to Latest API Specs.

--- semester_stats_report/studentclient.py
+++ semester_stats_report/studentclient.py
@@ -1,3 +1,4 @@
+from semester_stats_report.reciepts import ScoreReciept, StudentReciept
 from semester_stats_report.reports import StudentReport
 
 from .httpclient import BaseClient
@@ -9,19 +10,29 @@ class StudentClient(BaseClient):
         self.usn = usn
 
     def get(self):
-        return self._get("/{}".format(self.usn))
+        res = self._get("/{}".format(self.usn))
+        rec = StudentReciept.parse_obj(res)
+        return rec
 
     def get_scores(self):
-        return self._get("/{}/scores".format(self.usn))
+        res = self._get("/{}/scores".format(self.usn))
+        rec = [ScoreReciept.parse_obj(item) for item in res]
+        return rec
 
     def get_backlogs(self, sem: int):
-        return self._get("/{}/backlogs".format(self.usn), params={"sem": sem})
+        res = self._get("/{}/backlogs".format(self.usn), params={"sem": sem})
+        rec = [ScoreReciept.parse_obj(item) for item in res]
+        return rec
 
     def get_subject(self, subcode: str):
-        return self._get("/{}/subject/{}".format(self.usn, self.subcode))
+        res = self._get("/{}/subject/{}".format(self.usn, self.subcode))
+        rec = ScoreReciept.parse_obj(res)
+        return rec
 
     def update(self, report: StudentReport):
-        return self._post("/{}".format(self.usn), body=report.dict())
+        res = self._post("/{}".format(self.usn), body=report.dict())
+        return res
 
     def put(self, report: StudentReport):
-        return self._post("/", body=report.dict())
+        res = self._post("/", body=report.dict())
+        return res
