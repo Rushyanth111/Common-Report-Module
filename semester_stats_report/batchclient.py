b390f1571588a09f530c3b86585150c42133754c
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2020-10-11 18:09:58 +0530

    BatchClient: Updated to Latest Server API Routes.

--- semester_stats_report/batchclient.py
+++ semester_stats_report/batchclient.py
@@ -12,25 +12,10 @@ class BatchClient(BaseClient):
         super().__init__(url + "/batch")
         self.batch = str(batch)
 
-    def get(self, dept: str = None):
-        """Obtain the Batch Students
-
-        Args:
-            dept (str, optional): Department String. Defaults to None.
-
-        Returns:
-            Dict: The Result of the Operation
-        """
-        params = None if dept is None else {"dept": dept}
-        return self._get("/{}".format(self.batch), params=params)
-
     def get_scores(self, dept: str = None, sem: int = None):
         return self._get(
             "/{}/scores".format(self.batch), params={"dept": dept, "sem": sem}
         )
-
-    def get_usns(self, dept: str):
-        return self._get("/{}/usns".format(self.batch), params={"dept": dept})
 
     def get_scheme(self):
         return self._get("/{}/scheme".format(self.batch))
